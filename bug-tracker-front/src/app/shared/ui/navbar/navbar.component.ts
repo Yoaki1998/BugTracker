32ad722c32fa99cd62d81c3f2c2e7e6ea53273e3
Yoaki <yoakiniscoise@gmail.com>
2024-01-30 14:04:20 +0100

    add dynamic title inside navbar

--- bug-tracker-front/src/app/shared/ui/navbar/navbar.component.ts
+++ bug-tracker-front/src/app/shared/ui/navbar/navbar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit  {
+  public href: string = window.location.href;
+  public title: string = "";
+
+  ngOnInit(): void {
+    this.chooseTitle()
+  }
+
+  chooseTitle() {
+    if (this.href.includes('http://localhost:4200')) { this.title = "Home" }
+    if (this.href.includes('http://localhost:4200/project')) { this.title = "Projet" }
+    if (this.href.includes('http://localhost:4200/ticket')) { this.title = "Ticket" }
+    if (this.href.includes('http://localhost:4200/projects')) { this.title = "Liste des Projets" }
+    if (this.href.includes('http://localhost:4200/tickets')) { this.title = " Liste des Tickets" }
+  }
 
 }
